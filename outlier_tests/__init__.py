c8e74de7317820d633902654c65e4aaff840fad1
MatthewCorney <Matthew_corney@yahoo.co.uk>
2025-01-12 21:02:24 +0000

    feat: added top level functions

---
+++ outlier_tests/__init__.py
@@ -0,0 +1,7 @@
+from dixon import dixon_test
+from grubb import grubbs_test
+from rosner import rosner_test
+__all__ = ["dixon_test",
+           "grubbs_test",
+           "rosner_test"
+           ]
